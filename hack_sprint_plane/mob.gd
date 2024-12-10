e67d46425031d9e1a47357293f46be1d127d4c9a
ColeBiefrey <Cole.Kelsey@atlasschool.com>
2024-12-10 14:39:40 -0600

    updated mob handling and uploading oggs

--- hack_sprint_plane/mob.gd
+++ hack_sprint_plane/mob.gd
@@ -6,6 +6,7 @@ func _ready():
 	$AnimatedSprite2D.play(mob_types[randi() % mob_types.size()])
 	$AnimatedSprite2D.flip_h = true
 	
+	
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
 
